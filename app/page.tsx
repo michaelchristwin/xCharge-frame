2dab0b1980fb97e81d1990f6f8bfde25ec144663
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-03-03 12:53:58 +0100

    Fixed frameEmbed definition 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
-"use client";
+import PaymentForm from "@/components/PaymentForm";
+import { Metadata } from "next";
 
-import dynamic from "next/dynamic";
+const frame = {
+  version: "next",
+  imageUrl: `https://watt-a-frame.vercel.app/watt-a-frame.webp`,
+  button: {
+    title: "Launch Frame",
+    action: {
+      type: "launch_frame",
+      name: "Watt-A-Frame",
+      url: "https://watt-a-frame.vercel.app",
+      splashImageUrl: `https://watt-a-frame.vercel.app/lightbulb.png`,
+      splashBackgroundColor: "#f7f7f7",
+    },
+  },
+};
 
-const PaymentForm = dynamic(() => import("@/components/PaymentForm"), {
-  ssr: false,
-});
+export const revalidate = 300;
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Watt-A-Frame",
+    openGraph: {
+      title: "Watt-A-Frame",
+      description: "A farcaster frames for recharging M3ters with USDC.",
+    },
+    other: {
+      "fc:frame": JSON.stringify(frame),
+    },
+  };
+}
 
 export default function Home() {
   return (
