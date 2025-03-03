5bccf19c5e3cc3d9c4b8ff4f0b7ef2ea1dce59e4
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-03-03 12:42:07 +0100

    Fixed frameEmbed definition

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,47 @@
 "use client";
 
+import { Metadata } from "next";
 import dynamic from "next/dynamic";
-import Head from "next/head";
 
 const PaymentForm = dynamic(() => import("@/components/PaymentForm"), {
   ssr: false,
 });
+const appUrl = process.env.NEXT_PUBLIC_URL;
+
+const frame = {
+  version: "next",
+  imageUrl: `https://watt-a-frame.vercel.app/watt-a-frame.webp`,
+  button: {
+    title: "Launch Frame",
+    action: {
+      type: "launch_frame",
+      name: "Watt-A-Frame",
+      url: appUrl,
+      splashImageUrl: `https://watt-a-frame.vercel.app/lightbulb.png`,
+      splashBackgroundColor: "#f7f7f7",
+    },
+  },
+};
+
+export const revalidate = 300;
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Watt-A-Frame",
+    openGraph: {
+      title: "Watt-A-Frame",
+      description: "A farcaster frames for recharging M3ters with USDC.",
+    },
+    other: {
+      "fc:frame": JSON.stringify(frame),
+    },
+  };
+}
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <meta
-          name="fc:frame"
-          content={`{
-            "version": "next",
-            "imageUrl": "https://watt-a-frame.vercel.app/watt-a-frame.webp",
-            "button": {
-              "title": "Launch Frame",
-              "action": {
-                "type": "launch_frame",
-                "name": "Watt-A-Frame",
-                "url": "https://watt-a-frame.vercel.app",
-                "splashImageUrl": "https://watt-a-frame.vercel.app/lightbulb.png",
-                "splashBackgroundColor": "#ffffff"
-              }
-            }
-          }`}
-        />
-      </Head>
-      <main className="min-h-screen flex flex-col">
-        <PaymentForm />
-      </main>
-    </>
+    <main className="min-h-screen flex flex-col">
+      <PaymentForm />
+    </main>
   );
 }
